c7fb23488194822bfd805b5e7f406680e38e61ad
unknown <dharunjagan009@gmail.com>
2023-10-11 00:30:27 +0530

    Upadted2

--- Analysis.py
+++ Analysis.py
@@ -1,8 +1,13 @@
+import matplotlib
+matplotlib.use("Agg")  # Use the "Agg" backend for Matplotlib
+
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 from matplotlib.ticker import MaxNLocator
+from wordcloud import WordCloud
+from textblob import TextBlob
 
 # Create a function to load data with caching
 @st.cache(allow_output_mutation=True)
@@ -12,17 +17,17 @@ def load_data(file):
 def app():
     df = st.session_state.df
 
-        # Show the DataFrame
+    # Show the DataFrame
     st.subheader("Data")
 
     # Add a button to toggle data visibility
     if st.button("Toggle Data Display"):
         st.write(df)
 
-    
-    # Visualize the top 10 active members with a unique color
-    st.subheader("Top 10 Active Members")
-    top_active_members = df['Sender'].value_counts().head(10)
+    # Visualize the top N active members with a unique color
+    st.subheader("Top Active Members")
+    top_active_members_count = st.slider("Select the number of top active members", 1, 20, 10)
+    top_active_members = df['Sender'].value_counts().head(top_active_members_count)
     color_active = st.color_picker("Choose a color for Active Members", "#0074D9")
 
     # Create a professional-looking bar chart
@@ -30,8 +35,9 @@ def app():
     ax.bar(top_active_members.index, top_active_members, color=color_active)
     ax.set_xlabel("Members", fontsize=14)
     ax.set_ylabel("Message Count", fontsize=14)
-    ax.set_title("Top 10 Active Members", fontsize=16)
-    ax.tick_params(axis='x', labelrotation=45)  # Rotate x-axis labels for better readability
+    ax.set_title(f"Top {top_active_members_count} Active Members", fontsize=16)
+    ax.tick_params(axis='x', labelrotation=45, labelsize=12)  # Rotate x-axis labels and set label size
+    ax.tick_params(axis='y', labelsize=12)  # Set y-axis label size
     plt.tight_layout()  # Ensure proper spacing
     st.pyplot(fig)
 
@@ -47,7 +53,8 @@ def app():
     ax.set_xlabel("Senders", fontsize=14)
     ax.set_ylabel("<Media Omitted> Count", fontsize=14)
     ax.set_title("Top 10 <Media Omitted> on Message by Sender", fontsize=16)
-    ax.tick_params(axis='x', labelrotation=45)  # Rotate x-axis labels for better readability
+    ax.tick_params(axis='x', labelrotation=45, labelsize=12)  # Rotate x-axis labels and set label size
+    ax.tick_params(axis='y', labelsize=12)  # Set y-axis label size
     plt.tight_layout()  # Ensure proper spacing
     st.pyplot(fig)
 
@@ -57,18 +64,18 @@ def app():
     # Extract the day of the week from the date
     df['Day of Week'] = df['Date'].dt.strftime("%A")
 
-    # Visualize the most active days of the week with a unique color
+    # Visualize the top N most active days of the week with a unique color
     st.subheader("Most Active Days of the Week")
-    active_day = df['Day of Week'].value_counts()
-    top_active_days = active_day.head(10)
+    top_active_days_count = st.slider("Select the number of top active days", 1, 7, 5)
+    active_day = df['Day of Week'].value_counts().head(top_active_days_count)
 
     # Create a professional-looking bar chart
     fig, ax = plt.subplots(figsize=(10, 6))
-    tx = top_active_days.plot(kind='bar', color='dodgerblue', edgecolor='black', ax=ax)
+    tx = active_day.plot(kind='bar', color='dodgerblue', edgecolor='black', ax=ax)
 
     ax.set_xlabel('Day of Week', fontsize=14, fontweight='bold')
     ax.set_ylabel('Number of Messages', fontsize=14, fontweight='bold')
-    ax.set_title('Most Active Days in the Group', fontsize=18, fontweight='bold')
+    ax.set_title(f"Top {top_active_days_count} Active Days in the Group", fontsize=16, fontweight='bold')
 
     # Converting y-axis data to integer
     ax.yaxis.set_major_locator(MaxNLocator(integer=True))
@@ -85,8 +92,73 @@ def app():
     plt.tight_layout()
     st.pyplot(fig)
 
+    # Create a word cloud
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(' '.join(df['Message']))
 
+    # Display the word cloud
+    st.subheader("Word Cloud of Most Common Words")
+    st.image(wordcloud.to_array(), use_column_width=True)
 
+    # Message activity over time
+    st.subheader("Message Activity Over Time")
+    df['Date'] = pd.to_datetime(df['Date'])
+    message_activity_days = st.slider("Select the number of days for message activity", 1, 365, 30)
+    message_activity = df.resample(f'{message_activity_days}D', on='Date').size()  # Resample data by days
+
+    # Create a professional-looking line chart
+    fig, ax = plt.subplots(figsize=(10, 6))
+    message_activity.plot(kind='line', color='green', marker='o', ax=ax)
+    ax.set_xlabel("Date", fontsize=14)
+    ax.set_ylabel("Message Count", fontsize=14)
+    ax.set_title(f"Message Activity Over the Last {message_activity_days} Days", fontsize=16)
+    ax.grid(True)
+    plt.xticks(rotation=45, fontsize=12)  # Rotate x-axis labels and set label size
+    plt.yticks(fontsize=12)  # Set y-axis label size
+    plt.tight_layout()
+    st.pyplot(fig)
+
+    # Perform sentiment analysis on messages
+    def get_sentiment(text):
+        analysis = TextBlob(text)
+        if analysis.sentiment.polarity > 0:
+            return 'Positive'
+        elif analysis.sentiment.polarity < 0:
+            return 'Negative'
+        else:
+            return 'Neutral'
+
+    df['Sentiment'] = df['Message'].apply(get_sentiment)
+
+    st.subheader("Sentiment Analysis")
+    sentiment_counts = df['Sentiment'].value_counts()
+
+    # Create a professional-looking bar chart for sentiment analysis
+    fig, ax = plt.subplots(figsize=(8, 6))
+    sentiment_counts.plot(kind='bar', color='skyblue', edgecolor='black', ax=ax)
+    ax.set_xlabel("Sentiment", fontsize=14)
+    ax.set_ylabel("Count", fontsize=14)
+    ax.set_title("Sentiment Analysis", fontsize=16)
+    ax.tick_params(axis='x', rotation=0, labelsize=12)
+    ax.tick_params(axis='y', labelsize=12)
+    plt.tight_layout()
+    st.pyplot(fig)
+
+    # Message length vs. time
+    st.subheader("Message Length vs. Time")
+    df['Message Length'] = df['Message'].apply(len)
+
+    # Create a professional-looking line chart for message length vs. time
+    fig, ax = plt.subplots(figsize=(10, 6))
+    df[['Date', 'Message Length']].set_index('Date').plot(
+        kind='line', legend=False, color='purple', ax=ax
+    )
+    ax.set_xlabel("Date", fontsize=14)
+    ax.set_ylabel("Message Length", fontsize=14)
+    ax.set_title("Message Length vs. Time", fontsize=16)
+    plt.xticks(rotation=45, fontsize=12)  # Rotate x-axis labels and set label size
+    plt.yticks(fontsize=12)  # Set y-axis label size
+    plt.tight_layout()
+    st.pyplot(fig)
 
 if __name__ == "__main__":
     app()
