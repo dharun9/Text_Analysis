ef04d93a868d08be50aab4f9af8b6e9631c05126
unknown <dharunjagan009@gmail.com>
2023-10-11 11:46:35 +0530

    Upadted

--- Main.py
+++ Main.py
@@ -1,46 +1,42 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import Index, Home, Analysis, Link, Theme 
+import Index, Home, Analysis, Link, Theme
 
 # Set page configuration
 st.set_page_config(
     page_title="String Analysis",
 )
 
+class SessionState:
+    def __init__(self, **kwargs):
+        self._state = kwargs
+
+    def __getattr__(self, key):
+        return self._state.get(key, None)
+
+    def __setattr__(self, key, value):
+        self._state[key] = value
+
 class MultiApp:
-    
     def __init__(self):
         self.apps = []
-        self.session_state = {}
+        self.session_state = SessionState(selected_app="Home")
 
     def add_app(self, title, func):
         self.apps.append({
             "title": title,
             "function": func
         })
-        app_functions = {
-        "Index": Index.app,
-        "Home": Home.app,
-        "Analysis": Analysis.app,
-        "Link": Link.app,
-        "Theme": Theme.app,
-    }
-
-    # Set the default selected app
-    selected_app = st.sidebar.selectbox("Select App", list(app_functions.keys()))
-
-    # Run the selected app
-    app_functions[selected_app]()
 
     def run(self):
         # Get the selected app from session state or default to "Home"
-        selected_app = self.session_state.get("selected_app", "Home")
+        selected_app = self.session_state.selected_app
 
         # Create a sidebar with the option menu
         with st.sidebar:
             app = option_menu(
                 menu_title='Options',
-                options=['Index', 'Home', 'Analysis', 'Link','Theme'],
+                options=['Index', 'Home', 'Analysis', 'Link', 'Theme'],
                 icons=['house-fill', 'chat-fill', 'trophy-fill', 'chat-fill', 'info-circle-fill'],
                 menu_icon='chat-text-fill',
                 default_index=self.apps.index(next(app for app in self.apps if app["title"] == selected_app)),
@@ -54,7 +50,7 @@ class MultiApp:
             )
 
         # Store the selected app in session state
-        self.session_state["selected_app"] = app
+        self.session_state.selected_app = app
 
         # Run the selected app
         for item in self.apps:
@@ -70,7 +66,6 @@ multi_app.add_app("Home", Home.app)
 multi_app.add_app("Analysis", Analysis.app)
 multi_app.add_app("Link", Link.app)
 multi_app.add_app("Theme", Theme.app)
-
 
 # Run the selected app
 multi_app.run()
