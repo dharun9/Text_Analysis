b585855f5200ea08f452e0bcb27a6212cefdc9be
unknown <dharunjagan009@gmail.com>
2023-10-11 12:08:38 +0530

    Updated

--- Main.py
+++ Main.py
@@ -18,18 +18,31 @@ class MultiApp:
         })
 
     def run(self):
+        # Get the selected app from session state or default to "Home"
+        selected_app = st.session_state.get("selected_app", "Home")
+
         # Create a sidebar with the option menu
         with st.sidebar:
-            selected_app = option_menu(
+            app = option_menu(
                 menu_title='Options',
                 options=['Index', 'Home', 'Analysis', 'Link', 'Theme'],
                 icons=['house-fill', 'chat-fill', 'trophy-fill', 'chat-fill', 'info-circle-fill'],
                 menu_icon='chat-text-fill',
+                default_index=self.apps.index(next(app for app in self.apps if app["title"] == selected_app)),
+                styles={
+                    "container": {"padding": "5!important", "background-color": 'black'},
+                    "icon": {"color": "white", "font-size": "23px"},
+                    "nav-link": {"color": "white", "font-size": "20px", "text-align": "left", "margin": "0px", "---hover-color": "blue"},
+                    "nav-link-selected": {"background-color": "#02ab21"},
+                }
             )
+
+        # Store the selected app in session state
+        st.session_state.selected_app = app
 
         # Run the selected app
         for item in self.apps:
-            if item["title"] == selected_app:
+            if item["title"] == app:
                 item["function"]()
 
 # Create an instance of MultiApp
